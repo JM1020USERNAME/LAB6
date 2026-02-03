e2dea08561d6b793234e5ddec660bd9ab96925ca
JM1020USERNAME <fbyykpq9zw@privaterelay.appleid.com>
2026-02-03 13:56:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,14 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "0fd3fe62-0cf1-48a1-9244-91112d1f69fe"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "./setup_jenkins_azure.sh
+"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
